11838c7f72fcf537d173fc088a4e0289b2950abb
binwatch <h0w1eyu@proton.me>
2022-09-16 22:21:49 +0800

    view of online calculator

---
+++ src/components/content/login.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Base from './base';
+
+class Login extends Component {
+    state = {  } 
+    render() { 
+        return (
+            <Base>
+                Log in
+            </Base>
+        );
+    }
+}
+ 
+export default Login;
---
+++ src/components/content/register.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Base from './base';
+
+class Register extends Component {
+    state = {  } 
+    render() { 
+        return (
+            <Base>
+                Sign Up
+            </Base>
+        );
+    }
+}
+ 
+export default Register;
